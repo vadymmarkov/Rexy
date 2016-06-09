9e171680b7511acb2ae247f72a7b9722c92be417
Vadym Markov <markov.vadym@gmail.com>
2016-06-09 23:11:20 +0200

    Implement matches method

--- Sources/Rexy/Regex.swift
+++ Sources/Rexy/Regex.swift
@@ -10,10 +10,10 @@ public final class Regex {
    Creates a new regular expression
 
    - Parameter pattern: Regular expression to be compiled by the regcomp.
-   - Parameter options: Bitwise inclusive OR of 0 or more flags for the regcomp.
+   - Parameter flags: Bitwise inclusive OR of 0 or more flags for the regcomp.
    */
-  public init(pattern: String, options: CFlags = .extended) throws {
-    let result = regcomp(&preg, pattern, options.rawValue)
+  public init(pattern: String, flags: CFlags = .extended) throws {
+    let result = regcomp(&preg, pattern, flags.rawValue)
 
     guard result == 0 else {
       throw Error(from: result, preg: preg)
@@ -25,5 +25,18 @@ public final class Regex {
     regfree(&preg)
   }
 
-  
+  /**
+   Checks if given string matches regular expression.
+
+   - Parameter string: The string to be matched.
+   - Parameter flags: Flags controlling the behavior of the regexec.
+
+   - Returns: True if a match is found.
+   */
+  public func matches(_ string: String, flags: EFlags = []) -> Bool {
+    var offsets = [regmatch_t](repeating: regmatch_t(), count: 1)
+    let result = regexec(&preg, string, offsets.count, &offsets, flags.rawValue)
+
+    return result != 1
+  }
 }
