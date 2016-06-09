946d6b0190d12d194e4bd2ee92cfe36d6d40bc01
Vadym Markov <markov.vadym@gmail.com>
2016-06-09 22:58:29 +0200

    Introduce Regex

--- Sources/Rexy/Regex.swift
+++ Sources/Rexy/Regex.swift
@@ -1,10 +1,29 @@
+/**
+ POSIX Regular Expression
+ */
 public final class Regex {
 
+  /// Specifies the structure to receive the compiled output of the regcomp.
   var preg = regex_t()
 
-  
+  /**
+   Creates a new regular expression
 
+   - Parameter pattern: Regular expression to be compiled by the regcomp.
+   - Parameter options: Bitwise inclusive OR of 0 or more flags for the regcomp.
+   */
+  public init(pattern: String, options: CFlags = .extended) throws {
+    let result = regcomp(&preg, pattern, options.rawValue)
+
+    guard result == 0 else {
+      throw Error(from: result, preg: preg)
+    }
+  }
+
+  /// Destroy
   deinit {
     regfree(&preg)
   }
+
+  
 }
