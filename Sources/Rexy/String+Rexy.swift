83a9e374803082b69e954fb7e3b27769ec3c96e2
Vadym Markov <markov.vadym@gmail.com>
2016-06-16 00:29:53 +0200

    Implement string extensions

--- Sources/Rexy/String+Rexy.swift
+++ Sources/Rexy/String+Rexy.swift
@@ -1,9 +1,21 @@
-//
-//  String+Rexy.swift
-//  Rexy
-//
-//  Created by Vadym Markov on 16/06/16.
-//
-//
+extension String: RegexConvertible {
 
-import Foundation
+  public var regex: Regex? {
+    return try? Regex(pattern: self)
+  }
+}
+
+infix operator =~ {associativity left precedence 140}
+infix operator !~ {associativity left precedence 140}
+
+public func =~ (source: String, pattern: RegexConvertible?) -> Bool {
+  guard let matches = pattern?.regex?.matches(source) else {
+    return false
+  }
+
+  return matches
+}
+
+public func !~ (source:String, pattern: RegexConvertible?) -> Bool {
+  return !(source =~ pattern)
+}
